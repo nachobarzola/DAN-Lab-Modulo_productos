7e68ab5919136c5e388121a889eacaa33b2a6a06
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-05-30 14:01:29 -0300

    Se arreglaron los test y se cargo el archivo insomnia

--- src/test/java/dan/tp2021/productos/services/MoviemientosStockServiceImpTest.java
+++ src/test/java/dan/tp2021/productos/services/MoviemientosStockServiceImpTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -64,7 +66,7 @@ class MoviemientosStockServiceImpTest {
 		// Unidad
 		Unidad unidad1 = new Unidad("Entera");
 		unidadRepo.save(unidad1);
-		// Producto
+		// Producto 1
 		Producto producto1 = new Producto();
 		producto1.setDescripcion("Ladrillos Huecos");
 		producto1.setNombre("Ladrillos");
@@ -73,28 +75,60 @@ class MoviemientosStockServiceImpTest {
 		producto1.setStockMinimo(200);
 		producto1.setUnidad(unidad1);
 		producto1 = productoRepo.save(producto1);
-		//
-		DetallePedido detallePedido = new DetallePedido();
-		detallePedido.setProducto(producto1);
-		detallePedido.setCantidad(600);
-		detallePedido = detallePedidoRepo.save(detallePedido);
-		// -------------------------------------------------------
+		// Detalle pedido1
+		DetallePedido detallePedido1 = new DetallePedido();
+		detallePedido1.setProducto(producto1);
+		detallePedido1.setCantidad(600);
+		detallePedido1 = detallePedidoRepo.save(detallePedido1);
+		// Producto 2
+		Producto producto2 = new Producto();
+		producto2.setDescripcion("Ventana de metal con vidrio en el centro");
+		producto2.setNombre("Ventana");
+		producto2.setPrecio(5000.00);
+		producto2.setStockActual(100);
+		producto2.setStockMinimo(5);
+		producto2.setUnidad(unidad1);
+		producto2 = productoRepo.save(producto2);
+		// Detalle pedido2
+		DetallePedido detallePedido2 = new DetallePedido();
+		detallePedido2.setProducto(producto2);
+		detallePedido2.setCantidad(50);
+		detallePedido2 = detallePedidoRepo.save(detallePedido2);
 
-		Optional<MovimientosStock> optMovStock = movStockService.registrarMovimientoStock(detallePedido.getId());
-		assertTrue(optMovStock.isPresent());
-		assertEquals(0, optMovStock.get().getCantidadEntrada());
-		assertEquals(600, optMovStock.get().getCantidadSalida());
+		// -------------------------------------------------------
+		List<Integer> listaIdDetallePedidoDelPedido = new ArrayList<>();
+		listaIdDetallePedidoDelPedido.add(detallePedido1.getId());
+		listaIdDetallePedidoDelPedido.add(detallePedido2.getId());
+
+		List<Optional<MovimientosStock>> listaOptMovStock = movStockService
+				.registrarMovimientoStock(listaIdDetallePedidoDelPedido);
+		// ----------------------Movimiento 1--> Producto 1
+		// Para el movimiento stock 1
+		assertTrue(listaOptMovStock.get(0).isPresent());
+		assertEquals(0, listaOptMovStock.get(0).get().getCantidadEntrada());
+		assertEquals(600, listaOptMovStock.get(0).get().getCantidadSalida());
 		// vemos que no haya generado orden de provision
-		assertNull(optMovStock.get().getDetalleProvision());
+		assertNull(listaOptMovStock.get(0).get().getDetalleProvision());
+		// Vemos si actualizo el producto 1
+		Optional<Producto> optProducto1 = productoRepo.findById(producto1.getId());
+		assertEquals(400, optProducto1.get().getStockActual());
+		assertEquals(200, optProducto1.get().getStockMinimo()); // no tiene que cambiar
 
-		// Vemos si actualizo el producto
-		Optional<Producto> producto = productoRepo.findById(producto1.getId());
-		assertEquals(400, producto.get().getStockActual());
-		assertEquals(200, producto.get().getStockMinimo()); // no tiene que cambiar
+		// ----------------------Movimiento 2--> Producto 2
+		// Para el movimiento stock 2
+		assertTrue(listaOptMovStock.get(1).isPresent());
+		assertEquals(0, listaOptMovStock.get(1).get().getCantidadEntrada());
+		assertEquals(50, listaOptMovStock.get(1).get().getCantidadSalida());
+		// vemos que no haya generado orden de provision
+		assertNull(listaOptMovStock.get(1).get().getDetalleProvision());
+		// Vemos si actualizo el producto 2
+		Optional<Producto> optProducto2 = productoRepo.findById(producto2.getId());
+		assertEquals(50, optProducto2.get().getStockActual());
+		assertEquals(5, optProducto2.get().getStockMinimo()); // no tiene que cambiar
 	}
 
 	@Test
-	void resgistrarMovimientoStock_conGeneracionDeProvision() {
+	void resgistrarMovimientoStock_conGeneracionDeProvision_enProducto1() {
 		// Unidad
 		Unidad unidad1 = new Unidad("Entera");
 		unidadRepo.save(unidad1);
@@ -108,31 +142,63 @@ class MoviemientosStockServiceImpTest {
 		producto1.setUnidad(unidad1);
 		producto1 = productoRepo.save(producto1);
 		//
-		DetallePedido detallePedido = new DetallePedido();
-		detallePedido.setProducto(producto1);
-		detallePedido.setCantidad(850);
-		detallePedido = detallePedidoRepo.save(detallePedido);
+		DetallePedido detallePedido1 = new DetallePedido();
+		detallePedido1.setProducto(producto1);
+		detallePedido1.setCantidad(850);
+		detallePedido1 = detallePedidoRepo.save(detallePedido1);
+		// Producto 2
+		Producto producto2 = new Producto();
+		producto2.setDescripcion("Ventana de metal con vidrio en el centro");
+		producto2.setNombre("Ventana");
+		producto2.setPrecio(5000.00);
+		producto2.setStockActual(100);
+		producto2.setStockMinimo(5);
+		producto2.setUnidad(unidad1);
+		producto2 = productoRepo.save(producto2);
+		// Detalle pedido2
+		DetallePedido detallePedido2 = new DetallePedido();
+		detallePedido2.setProducto(producto2);
+		detallePedido2.setCantidad(50);
+		detallePedido2 = detallePedidoRepo.save(detallePedido2);
 		// -------------------------------------------------------
+		List<Integer> listaIdDetallePedidoDelPedido = new ArrayList<>();
+		listaIdDetallePedidoDelPedido.add(detallePedido1.getId());
+		listaIdDetallePedidoDelPedido.add(detallePedido2.getId());
 
-		Optional<MovimientosStock> optMovStock = movStockService.registrarMovimientoStock(detallePedido.getId());
-		assertTrue(optMovStock.isPresent());
-		assertEquals(900, optMovStock.get().getCantidadEntrada());
-		assertEquals(850, optMovStock.get().getCantidadSalida());
+		List<Optional<MovimientosStock>> listaOptMovStock = movStockService
+				.registrarMovimientoStock(listaIdDetallePedidoDelPedido);
+
+		// ----------------------Movimiento 1--> Producto 1
+		assertTrue(listaOptMovStock.get(0).isPresent());
+		assertEquals(900, listaOptMovStock.get(0).get().getCantidadEntrada());
+		assertEquals(850, listaOptMovStock.get(0).get().getCantidadSalida());
 		// vemos que Si se haya generado orden de provision
-		assertNotNull(optMovStock.get().getDetalleProvision());
-		assertNotNull(optMovStock.get().getDetalleProvision().getProvision());
+		assertNotNull(listaOptMovStock.get(0).get().getDetalleProvision());
+		assertNotNull(listaOptMovStock.get(0).get().getDetalleProvision().getProvision());
 		// Chequeamos el valor de aprovionamiento
-		assertEquals(900, optMovStock.get().getDetalleProvision().getCantidad());
+		assertEquals(900, listaOptMovStock.get(0).get().getDetalleProvision().getCantidad());
 
 		// Vemos si actualizo el producto
-		Optional<Producto> producto = productoRepo.findById(producto1.getId());
-		assertEquals(1050, producto.get().getStockActual());
-		assertEquals(200, producto.get().getStockMinimo()); // no tiene que cambiar
+		Optional<Producto> optProducto1 = productoRepo.findById(producto1.getId());
+		assertEquals(1050, optProducto1.get().getStockActual());
+		assertEquals(200, optProducto1.get().getStockMinimo()); // no tiene que cambiar
+
+		// ----------------------Movimiento 2--> Producto 2
+		// Para el movimiento stock 2
+		assertTrue(listaOptMovStock.get(1).isPresent());
+		assertEquals(0, listaOptMovStock.get(1).get().getCantidadEntrada());
+		assertEquals(50, listaOptMovStock.get(1).get().getCantidadSalida());
+		// vemos que no haya generado orden de provision
+		assertNull(listaOptMovStock.get(1).get().getDetalleProvision());
+		// Vemos si actualizo el producto 2
+		Optional<Producto> optProducto2 = productoRepo.findById(producto2.getId());
+		assertEquals(50, optProducto2.get().getStockActual());
+		assertEquals(5, optProducto2.get().getStockMinimo()); // no tiene que cambiar
 
 	}
 
 	@Test
-	void resgistrarMovimientoStock_conGeneracionDeProvision_2() {
+	void resgistrarMovimientoStock_conGeneracionDeProvision_enAmbosProductos() {
 		// Unidad
 		Unidad unidad1 = new Unidad("Entera");
 		unidadRepo.save(unidad1);
@@ -146,26 +212,62 @@ class MoviemientosStockServiceImpTest {
 		producto1.setUnidad(unidad1);
 		producto1 = productoRepo.save(producto1);
 		//
-		DetallePedido detallePedido = new DetallePedido();
-		detallePedido.setProducto(producto1);
-		detallePedido.setCantidad(2000);
-		detallePedido = detallePedidoRepo.save(detallePedido);
+		DetallePedido detallePedido1 = new DetallePedido();
+		detallePedido1.setProducto(producto1);
+		detallePedido1.setCantidad(2000);
+		detallePedido1 = detallePedidoRepo.save(detallePedido1);
+		// Producto 2
+		Producto producto2 = new Producto();
+		producto2.setDescripcion("Ventana de metal con vidrio en el centro");
+		producto2.setNombre("Ventana");
+		producto2.setPrecio(5000.00);
+		producto2.setStockActual(100);
+		producto2.setStockMinimo(5);
+		producto2.setUnidad(unidad1);
+		producto2 = productoRepo.save(producto2);
+		// Detalle pedido2
+		DetallePedido detallePedido2 = new DetallePedido();
+		detallePedido2.setProducto(producto2);
+		detallePedido2.setCantidad(98);
+		detallePedido2 = detallePedidoRepo.save(detallePedido2);
 		// -------------------------------------------------------
+		List<Integer> listaIdDetallePedidoDelPedido = new ArrayList<>();
+		listaIdDetallePedidoDelPedido.add(detallePedido1.getId());
+		listaIdDetallePedidoDelPedido.add(detallePedido2.getId());
 
-		Optional<MovimientosStock> optMovStock = movStockService.registrarMovimientoStock(detallePedido.getId());
-		assertTrue(optMovStock.isPresent());
-		assertEquals(1800, optMovStock.get().getCantidadEntrada());
-		assertEquals(2000, optMovStock.get().getCantidadSalida());
+		List<Optional<MovimientosStock>> listaOptMovStock = movStockService
+				.registrarMovimientoStock(listaIdDetallePedidoDelPedido);
+
+		// ----------------------Movimiento 1--> Producto 1
+		assertTrue(listaOptMovStock.get(0).isPresent());
+		assertEquals(1800, listaOptMovStock.get(0).get().getCantidadEntrada());
+		assertEquals(2000, listaOptMovStock.get(0).get().getCantidadSalida());
 		// vemos que Si se haya generado orden de provision
-		assertNotNull(optMovStock.get().getDetalleProvision());
-		assertNotNull(optMovStock.get().getDetalleProvision().getProvision());
+		assertNotNull(listaOptMovStock.get(0).get().getDetalleProvision());
+		assertNotNull(listaOptMovStock.get(0).get().getDetalleProvision().getProvision());
 		// Chequeamos el valor de aprovionamiento
-		assertEquals(1800, optMovStock.get().getDetalleProvision().getCantidad());
+		assertEquals(1800, listaOptMovStock.get(0).get().getDetalleProvision().getCantidad());
 
 		// Vemos si actualizo el producto
 		Optional<Producto> producto = productoRepo.findById(producto1.getId());
 		assertEquals(800, producto.get().getStockActual());
 		assertEquals(200, producto.get().getStockMinimo()); // no tiene que cambiar
+
+		// ----------------------Movimiento 2--> Producto 2
+		// Para el movimiento stock 2
+		assertTrue(listaOptMovStock.get(1).isPresent());
+		assertEquals(900, listaOptMovStock.get(1).get().getCantidadEntrada());
+		assertEquals(98, listaOptMovStock.get(1).get().getCantidadSalida());
+		// vemos que se haya generado orden de provision
+		assertNotNull(listaOptMovStock.get(1).get().getDetalleProvision());
+		assertNotNull(listaOptMovStock.get(1).get().getDetalleProvision().getProvision());
+		// Chequeamos el valor de aprovionamiento
+		assertEquals(900, listaOptMovStock.get(1).get().getDetalleProvision().getCantidad());
+
+		// Vemos si actualizo el producto 2
+		Optional<Producto> optProducto2 = productoRepo.findById(producto2.getId());
+		assertEquals(902, optProducto2.get().getStockActual());
+		assertEquals(5, optProducto2.get().getStockMinimo()); // no tiene que cambiar
 
 	}
 
