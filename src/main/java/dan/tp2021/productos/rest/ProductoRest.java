1b83912d29a8720e79cfc5fb4485742c481ded09
Franco Perino <fperino@frsf.utn.edu.ar>
2021-09-02 11:53:07 -0300

    Agregado CrossOrigin

--- src/main/java/dan/tp2021/productos/rest/ProductoRest.java
+++ src/main/java/dan/tp2021/productos/rest/ProductoRest.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/producto")
+@CrossOrigin("*")
 public class ProductoRest {
 	// Material es quivalente a producto
 
